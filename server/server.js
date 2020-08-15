835d4e7e646464f982b3c8eac79b69d9b10548a3
Jordan McCollam <jordanmccollam@Jordans-MacBook-Pro.local>
2020-08-15 16:38:58 -0400

    Finished basic auth setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-  
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const session = require('express-session')
 const dbConnection = require('./database') 
-
+const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
 const app = express()
 const PORT = 8080
@@ -24,6 +23,7 @@ app.use(bodyParser.json())
 app.use(
 	session({
 		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
 		resave: false, //required
 		saveUninitialized: false //required
 	})
